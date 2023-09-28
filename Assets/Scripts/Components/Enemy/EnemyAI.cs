03e320d4bc787d18f0c0401286896146b66a3fe2
UESTC-xdd <821378157@qq.com>
2023-09-28 13:58:00 -0400

    Enemy Ai Script

--- Assets/Scripts/Components/Enemy/EnemyAI.cs
+++ Assets/Scripts/Components/Enemy/EnemyAI.cs
@@ -14,17 +14,22 @@ public class EnemyAI : MonoBehaviour
     [SerializeField]
     private Animator m_Anim;
 
+    public Transform[] PatrolPoints;
+    public float PatrolIntervalDelay;
+    private int PatrolIndex;
+
     public bool IsPlayerInView
     {
         get { return m_IsPlayerInView; }
         set { m_IsPlayerInView = value; }
     }
 
-    private int Anim_HasTarget;
+    private int Anim_IsMoving;
+    private bool IsPatroling;
 
     private void Awake()
     {
-        Anim_HasTarget = Animator.StringToHash("HasTarget");
+        Anim_IsMoving = Animator.StringToHash("IsMoving");
     }
 
     private void Update()
@@ -36,11 +41,44 @@ public class EnemyAI : MonoBehaviour
     public void KeepFollowingTarget()
     {
         if(IsPlayerInView)
+        {
             m_Agent.SetDestination(FollowTarget.position);
+            StopCoroutine(PatrolCou());
+            IsPatroling = false;
+        }
+        else
+        {
+            if(!IsPatroling)
+            {
+                StartCoroutine(PatrolCou());
+            }
+        }
+    }
+
+    private IEnumerator PatrolCou()
+    {
+        m_Agent.SetDestination(PatrolPoints[PatrolIndex].position);
+        IsPatroling = true;
+        yield return new WaitUntil(() => Vector3.Distance(transform.position, PatrolPoints[PatrolIndex].position) <= m_Agent.stoppingDistance);
+        Debug.Log("Reach Patrol Point");
+        yield return new WaitForSeconds(PatrolIntervalDelay);
+        IsPatroling = false;
+        PatrolIndex++;
+        if(PatrolIndex>=PatrolPoints.Length)
+        {
+            PatrolIndex = 0;
+        }
     }
 
     private void UpdateAnim()
     {
-        m_Anim.SetBool(Anim_HasTarget, IsPlayerInView);
+        if(IsPlayerInView || m_Agent.velocity.sqrMagnitude>0.5f)
+        {
+            m_Anim.SetBool(Anim_IsMoving, true);
+        }
+        else
+        {
+            m_Anim.SetBool(Anim_IsMoving, false);
+        }
     }
 }
